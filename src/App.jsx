69393be615524923b15af3677aa05f98df5b346c
Aditya Singh <adityasingh917023@gmail.com>
2026-06-15 22:39:27 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Dashboard from './pages/Dashboard'
 import {Routes,Route} from 'react-router-dom'
 import ProtectedRoute from './ProtectedRoute'
 import { useState } from 'react'
-
+import { Routes, Route, Navigate } from "react-router-dom";
 function App() {
    const [darkMode, setDarkMode] = useState(false);
   return (
     <div className={darkMode ? "dark" : ""}>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path='/login' element = {<Login/>} />
       <Route path='/register' element = {<Register/>} />
       <Route path='/dashboard' element = {
